10872836737aa156d5f678d3c1e0f26347a76c67
samuel <samuelnrojas4@gmail.com>
2026-03-16 21:13:29 -0500

    Primer commit -proyecto conciertos

---
+++ src/main/java/TicketYa/Ticketya/gestion/Evento.java
@@ -0,0 +1,10 @@
+package TicketYa.Ticketya.gestion;
+
+import java.util.Date;
+
+public class Evento {
+    private String nombre;
+    private Date fecha;
+    private Date hora;
+    private String
+}
