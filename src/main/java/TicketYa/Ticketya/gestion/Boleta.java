66d1ca25745c922280b0003e90dea530d95e1cae
samuel <samuelnrojas4@gmail.com>
2026-03-16 22:44:52 -0500

    Se corrigio la clase de zona

--- src/main/java/TicketYa/Ticketya/gestion/Boleta.java
+++ src/main/java/TicketYa/Ticketya/gestion/Boleta.java
@@ -1,7 +1,14 @@
 package TicketYa.Ticketya.gestion;
 
 public class Boleta {
+    public static final int MaximoBoletas = 10000;
+
     private int codigo;
     private String estado;
     private int totalDeBoletas;
+
+    public Boleta(int totalDeBoletas){
+        this.totalDeBoletas = 0;
+
+    }
 }
