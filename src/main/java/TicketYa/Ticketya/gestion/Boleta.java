4a98310844f7703133620f3dd6ffb98d56fc300b
samuel <samuelnrojas4@gmail.com>
2026-03-16 22:53:38 -0500

    Agregue constuctores a la clase de Boleta

--- src/main/java/TicketYa/Ticketya/gestion/Boleta.java
+++ src/main/java/TicketYa/Ticketya/gestion/Boleta.java
@@ -1,14 +1,42 @@
 package TicketYa.Ticketya.gestion;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+
 public class Boleta {
-    public static final int MaximoBoletas = 10000;
 
-    private int codigo;
-    private String estado;
-    private int totalDeBoletas;
+    private List<String> localidad;
+    private int estado;
+    private int totalBoletas;
+    private int botelasDisponibles;
+    private double precio;
 
-    public Boleta(int totalDeBoletas){
-        this.totalDeBoletas = 0;
-
+    public Boleta() {
     }
+
+    public Boleta(List<String> localidad, int estado, int totalBoletas, int botelasDisponibles, double precio) {
+        this.localidad = localidad;
+        this.estado = estado;
+        this.totalBoletas = totalBoletas;
+        this.botelasDisponibles = botelasDisponibles;
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Boleta{" +
+                "localidad=" + localidad +
+                ", estado=" + estado +
+                ", totalBoletas=" + totalBoletas +
+                ", botelasDisponibles=" + botelasDisponibles +
+                ", precio=" + precio +
+                '}';
+    }
+
 }
