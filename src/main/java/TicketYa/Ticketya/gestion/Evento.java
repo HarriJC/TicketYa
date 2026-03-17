5684a1b869aa4358feb4b4849ab7b5a22d917442
samuel <samuelnrojas4@gmail.com>
2026-03-16 23:26:57 -0500

    Modificando clase de Evento

--- src/main/java/TicketYa/Ticketya/gestion/Evento.java
+++ src/main/java/TicketYa/Ticketya/gestion/Evento.java
@@ -2,26 +2,28 @@ package TicketYa.Ticketya.gestion;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.Date;
+
 @Getter
 @Setter
-
-
 public class Evento {
+
     private String nombreEvento;
     private Date fecha;
     private Date hora;
     private String lugar;
     private String patrocinador;
 
-    public Evento(String nombreEvento, Date fecha, Date hora, String patrocinador) {
-        this.nombreEvento = nombreEvento;
-        this.fecha=fecha;
-        this.hora=hora;
-        this.patrocinador=patrocinador;
-
-
+    // Constructor vacío
+    public Evento() {
     }
 
+    // Constructor con parámetros
+    public Evento(String nombreEvento, Date fecha, Date hora, String lugar, String patrocinador) {
+        this.nombreEvento = nombreEvento;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.lugar = lugar;
+        this.patrocinador = patrocinador;
+    }
 }
